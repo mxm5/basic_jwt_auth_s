998a93e99c28e80f2e91660cd0864827dd8b319c
mohammadian <mohammadianmx@gmail.com>
2022-05-07 18:54:54 +0430

    FIXED ERROR 1 reversing debugging mode to clean

--- src/main/java/com/example/sayehwebservices/SayehWebservicesApplication.java
+++ src/main/java/com/example/sayehwebservices/SayehWebservicesApplication.java
@@ -1,9 +1,12 @@
 package com.example.sayehwebservices;
 
 import com.example.sayehwebservices.domain.Merchant;
+import com.example.sayehwebservices.domain.khanevar;
 import com.example.sayehwebservices.repository.KhanevarRepository;
 import com.example.sayehwebservices.repository.MerchantRepository;
 import com.example.sayehwebservices.repository.UsersRepository;
+import com.example.sayehwebservices.services.KhanevarService;
+import com.example.sayehwebservices.services.dto.FamilyMembersRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,12 +15,32 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SayehWebservicesApplication  {
+public class SayehWebservicesApplication /*implements CommandLineRunner*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(SayehWebservicesApplication.class, args);
     }
 
-
+//    @Autowired
+//    UsersRepository usersRepository;
+//
+//    @Autowired
+//    MerchantRepository merchantRepository;
+//
+//    @Autowired
+//    KhanevarRepository khanevarRepository;
+//
+//    @Autowired
+//    KhanevarService khanevarService;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        //  2529576092   has 6 members
+//        Pageable page=PageRequest.of(0,10);
+//        FamilyMembersRes familyMembersByParentNationalCode = khanevarService.getFamilyMembersByParentNationalCode("2529576092");
+//        familyMembersByParentNationalCode.getFamilyMembers().forEach(System.out::println);
+//    }
 }
